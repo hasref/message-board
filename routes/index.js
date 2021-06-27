d36c28fc33d84b2b79525917a63fb41e4df3b108
hasan ashraf <hasan_ashraf@fastmail.com>
2021-06-27 04:15:21 +0200

    add some sample messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const sampleMessages = [
+  {
+    text: "Where does the road lead?",
+    author: "Wise Philosopher",
+    date: new Date(),
+  },
+  {
+    text: "I peered into the abyss and the abyss stared past.",
+    author: "Contemporary Philosopher",
+    date: new Date(),
+  },
+  {
+    text: "In the beginning the universe was created. This has made a lot of people very angry and has generally been regarded as a bad move.",
+    author: "Douglas",
+    date: new Date(),
+  },
+  {
+    text: "Don't Panic. Sincerely, Rust.",
+    author: "Steve",
+    date: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Messages", messages: sampleMessages });
 });
 
 module.exports = router;
---
+++ todos.md
@@ -0,0 +1,3 @@
+1. Show date in terms of 1,2,3 hours ago. If greater than 24, then just show the time.
+2. Make flex containers responsive at smaller screen sizes.
+3. Make prettier.
